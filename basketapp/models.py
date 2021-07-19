a48727842e0d5cb54383ad5e31c2cf08a46a8fab
Люция <lvaleeva87@gmail.com>
2021-07-19 18:20:04 +0500

    cached_property

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,3 +1,4 @@
+from cryptography.utils import cached_property
 from django.db import models
 from django.conf import settings
 from mainapp.models import Product
@@ -13,41 +14,42 @@ from mainapp.models import Product
 
 
 class Basket(models.Model):
-    #objects = BasketQuerySet.as_manager()
+    # objects = BasketQuerySet.as_manager()
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
     add_datetime = models.DateTimeField(verbose_name='время добавления', auto_now_add=True)
-    
-    
+
     def _get_product_cost(self):
         "return cost of all products this type"
         return self.product.price * self.quantity
-    
+
     product_cost = property(_get_product_cost)
-    
-    
+
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
+
     def get_total_quantity(self):
         "return total quantity for user"
-        _items = Basket.objects.filter(user=self.user)
-        _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
-        return _totalquantity
-        
-   # total_quantity = property(get_total_quantity)
-    
-    
+        # _items = Basket.objects.filter(user=self.user)
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.quantity, _items)))
+
+    # total_quantity = property(get_total_quantity)
+
     def get_total_cost(self):
         "return total cost for user"
-        _items = Basket.objects.filter(user=self.user)
-        _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
-        return _totalcost
-        
-    #total_cost = property(get_total_cost)
+        # _items = Basket.objects.filter(user=self.user)
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.product_cost, _items)))
+
+    # total_cost = property(get_total_cost)
 
     @staticmethod
     def get_items(user):
-        return  Basket.objects.filter(user=user).order_by('product__category')
+        return Basket.objects.filter(user=user).order_by('product__category')
 
     @staticmethod
     def get_product(user, product):
