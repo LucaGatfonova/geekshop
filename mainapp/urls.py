89a808e686873b477ebfd2160aa4717a368029aa
Люция <lvaleeva87@gmail.com>
2021-07-20 19:42:15 +0500

    memcached cache_page

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,12 +2,15 @@
 
 import mainapp.views as mainapp
 from django.urls import re_path
+from django.views.decorators.cache import cache_page
 
 app_name="mainapp"
 
 urlpatterns = [
     re_path(r'^$', mainapp.products, name='index'),
     re_path(r'^category/(?P<pk>\d+)/$', mainapp.products, name='category'),
+    re_path(r'^category/(?P<pk>\d+)/ajax/$', cache_page(3600)(mainapp.products_ajax)),
+
     re_path(r'^product/(?P<pk>\d+)/$', mainapp.product, name='product'),
 
     re_path(r'^category/(?P<pk>\d+)/page/(?P<page>\d+)/$', mainapp.products, name='page'),
