581b9607292c5b894811c4b8564ceab93fdc8f31
Люция <lvaleeva87@gmail.com>
2021-07-21 18:15:44 +0500

    F-object

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -26,7 +26,7 @@ DEBUG = True
 
 if DEBUG:
     def show_toolbar(request):
-        return True
+        return False
 
 
     DEBUG_TOOLBAR_CONFIG = {
@@ -125,20 +125,20 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'geekshop',
-        'USER': 'postgres'
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'geekshop',
+#         'USER': 'postgres'
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
